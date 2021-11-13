d095d5567c916968ec70a59e303edbffae76e455
Owais <oowais@gmail.com>
2021-11-13 07:25:23 +0100

    updated model to fetch thumbnail of products

--- product/models.py
+++ product/models.py
@@ -4,6 +4,7 @@ from PIL import Image
 from django.core.files import File
 from django.db import models
 
+
 class Category(models.Model):
     name = models.CharField(max_length=255)
     slug = models.SlugField()
@@ -19,11 +20,12 @@ class Category(models.Model):
 
 
 class Product(models.Model):
-    category = models.Foreignkey(Category, related_name='products', on_delete=models.CASCADE)
+    category = models.ForeignKey(
+        Category, related_name='products', on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
     slug = models.SlugField()
     description = models.TextField(blank=True, null=True)
-    price = models.DecimelField(max_digits=6, decimel_places=2)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
     image = models.ImageField(upload_to='uploads/', blank=True, null=True)
     thumbnail = models.ImageField(upload_to='uploads/', blank=True, null=True)
     date_added = models.DateTimeField(auto_now_add=True)
@@ -42,4 +44,26 @@ class Product(models.Model):
             return 'http:127.0.0.1:8000' + self.image.url
         return ''
 
-    
+    def get_thumbnail(self):
+        if self.thumbnail:
+            return 'http:127.0.0.1:8000' + self.thumbnail.url
+        else:
+            if self.image:
+                self.thumbnail = self.make_thumbail(self.image)
+                self.save()
+
+                return 'http:127.0.0.1:8000' + self.thumbnail.url
+            else:
+                return ''
+
+    def make_thumbail(self, image, size=(300, 200)):
+        img = Image.open(iamge)
+        img.convert('RGB')
+        img.thumbnail(size)
+
+        thumb_io = BytesIO()
+        img.save(thumb_io, 'JPEG', quality=85)
+
+        thumbnail = File(thumb_io, name=image.name)
+
+        return thumbnail
