4d3d21753a54fd631c5260484ad22e3feb1c1f02
Owais <oowais@gmail.com>
2021-11-13 07:03:06 +0100

    first commit

---
+++ product/models.py
@@ -0,0 +1,45 @@
+from io import BytesIO
+from PIL import Image
+
+from django.core.files import File
+from django.db import models
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    slug = models.SlugField()
+
+    class Meta:
+        ordering = ('name',)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return f'/{self.slug}/'
+
+
+class Product(models.Model):
+    category = models.Foreignkey(Category, related_name='products', on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    slug = models.SlugField()
+    description = models.TextField(blank=True, null=True)
+    price = models.DecimelField(max_digits=6, decimel_places=2)
+    image = models.ImageField(upload_to='uploads/', blank=True, null=True)
+    thumbnail = models.ImageField(upload_to='uploads/', blank=True, null=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ('-date_added',)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return f'/{self.category.slug}/{self.slug}/'
+
+    def get_image(self):
+        if self.image:
+            return 'http:127.0.0.1:8000' + self.image.url
+        return ''
+
+    
