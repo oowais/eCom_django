2d4867317e5718fa0adc75598326961be588e81b
Owais <oowais@gmail.com>
2021-11-13 13:02:57 +0100

    Ability to get categories now

--- product/views.py
+++ product/views.py
@@ -3,8 +3,8 @@ from django.http import Http404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
-from .models import Product
-from .serializers import ProductSerializer
+from .models import Product, Category
+from .serializers import ProductSerializer, CategorySerializer
 
 
 class LatestProductsList(APIView):
@@ -14,7 +14,7 @@ class LatestProductsList(APIView):
         return Response(serializer.data)
 
 
-class ProductDetails(APIView):
+class ProductDetail(APIView):
     def get_object(self, category_slug, product_slug):
         try:
             return Product.objects.filter(category__slug=category_slug).get(slug=product_slug)
@@ -24,4 +24,17 @@ class ProductDetails(APIView):
     def get(self, request, category_slug, product_slug, format=None):
         product = self.get_object(category_slug, product_slug)
         serializer = ProductSerializer(product)
+        return Response(serializer.data)
+
+
+class CategoryDetail(APIView):
+    def get_object(self, category_slug):
+        try:
+            return Category.objects.get(slug=category_slug)
+        except Category.DoesNotExist:
+            raise Http404
+
+    def get(self, request, category_slug, format=None):
+        category = self.get_object(category_slug)
+        serializer = CategorySerializer(category)
         return Response(serializer.data)
